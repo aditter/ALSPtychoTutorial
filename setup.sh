680600aa3c64b10e79a051310e2b918468ab320a
aditter <aditter@login05.chn.perlmutter.nersc.gov>
2025-08-13 00:21:57 -0700

    Finished DataAnalysis.ipynb, removed InitialTest.ipynb, updated setup script to include scikit-learn

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 module load python
 conda create -n ptycho-tutorial ipykernel --yes
 conda activate ptycho-tutorial
-yes | pip install cdtools-py scikit-image
+yes | pip install cdtools-py scikit-image scikit-learn
 python -m ipykernel install --user --name ptycho-tutorial --display-name ptycho-tutorial
